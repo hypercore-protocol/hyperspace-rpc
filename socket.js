d7013620cf430398250f0d01eaa5cb2aeb6c2010
Mathias Buus <mathiasbuus@gmail.com>
2020-07-13 17:32:23 +0200

    use os.tmpdir

--- socket.js
+++ socket.js
@@ -2,5 +2,5 @@ const os = require('os')
 
 module.exports = function getSocketPath (name) {
   name = name || 'hyperspace'
-  return os.platform() !== 'win32' ? `/tmp/${name}.sock` : `\\\\.\\pipe\\${name}`
+  return os.platform() !== 'win32' ? `${os.tmpdir()}/${name}.sock` : `\\\\.\\pipe\\${name}`
 }
