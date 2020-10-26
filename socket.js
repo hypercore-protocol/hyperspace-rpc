7158f8fe013f6151b55f47a9f6f9957a1433cad6
Mathias Buus <mathiasbuus@gmail.com>
2020-10-26 11:30:52 +0100

    decouple net to allow websockets and more

--- socket.js
+++ socket.js
@@ -5,7 +5,12 @@ function getSocketPath (name) {
   return os.platform() !== 'win32' ? `${os.tmpdir()}/${name}.sock` : `\\\\.\\pipe\\${name}`
 }
 
+function isEventEmitter (o) {
+  return typeof o === 'object' && o !== null && typeof o.on === 'function'
+}
+
 module.exports = function getNetworkOptions (opts) {
+  if (isEventEmitter(opts)) return opts
   if (!opts.host && !opts.port) return getSocketPath()
   if (opts.host && !opts.port) return getSocketPath(opts.host)
   return { host: opts.host, port: opts.port }
